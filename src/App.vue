edd1d1e4dad306c49bc12e33bed819d9f1022140
erhays <erhays@gmail.com>
2018-12-29 03:09:12 -0700

    scaffolded

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,30 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>|
-      <router-link to="/about">About</router-link>|
-      <router-link to="/work">Work</router-link>|
-      <router-link to="/blog">Blog</router-link>
+    <div
+      class="uk-navbar-container tm-navbar-container uk-sticky uk-active uk-sticky-below uk-sticky-fixed"
+    >
+      <div class="uk-container uk-container-expand">
+        <nav class="uk-navbar-container uk-navbar-transparent" uk-navbar>'
+          <div class="uk-navbar-left">
+            <span uk-navbar-item uk-logo uk-icon="icon: heart"></span>
+            <span class="uk-flex" uk-navbar-item>
+              <router-link class="uk-margin-left" to="/">Home</router-link>
+              <router-link class="uk-margin-left" to="/about">About</router-link>
+              <router-link class="uk-margin-left" to="/work">Work</router-link>
+              <router-link to="/blog">Blog</router-link>
+            </span>
+          </div>
+        </nav>
+      </div>
     </div>
     <router-view/>
+    <footer
+      class="uk-section uk-background-secondary tm-section-primary uk-padding-remove-bottom uk-position-bottom"
+    >
+      <div class="uk-container uk-text-center">
+        <p class="uk-text-muted">Built with Vue.js, UIKit + Buttercms.</p>
+      </div>
+    </footer>
   </div>
 </template>
 
--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,13 +1,10 @@
 <template>
   <div class="hero-section">
-    <main class="uk-section uk-section-default">
-      <router-view></router-view>
-    </main>
-    <footer class="uk-section tm-section-primary uk-section-xlarge uk-padding-remove-bottom">
-      <div class="uk-container uk-text-center">
-        <p class="uk-text-muted">Built with Vue.js, UIKit + Buttercms.</p>
+    <div class="uk-section uk-section-default">
+      <div class="uk-container">
+        {...Hero Section...}
       </div>
-    </footer>
+    </div>
   </div>
 </template>
 
