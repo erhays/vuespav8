e43ae92a8b336bd43e4a2c4676377542f5417797
erhays <erhays@gmail.com>
2018-12-29 00:38:41 -0700

    sass-loader

--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,10 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  chainWebpack: config => {
-    const types = ["vue-modules", "vue", "normal-modules", "normal"];
-    types.forEach(type =>
-      addStyleResource(config.module.rule("scss").oneOf(type))
-    );
+  pluginOptions: {
+    'style-resource-loader': {
+      preProcessor: 'scss',
+      patterns: [path.resolve(__dirname, './src/sass/main.scss')]
+    }
   }
 };
-
-function addStyleResource(rule) {
-  rule
-    .use("style-resource")
-    .loader("style-resources-loader")
-    .options({
-      patterns: [path.resolve(__dirname, "./src/sass/main.scss")]
-    });
-}
