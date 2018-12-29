a36e5c1504baa380bfbbfec4348f85ed0ee4475a
erhays <erhays@gmail.com>
2018-12-28 22:12:33 -0700

    +loaders

--- vue.config.js
+++ vue.config.js
@@ -12,7 +12,7 @@ function addStyleResource(rule) {
     .loader('style-resources-loader')
     .options({
       patterns: [
-        path.resolve(__dirname, './src/styles/main.scss'),
+        path.resolve(__dirname, './src/sass/main.scss'),
       ],
     })
 }
