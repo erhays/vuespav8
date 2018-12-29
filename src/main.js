a143532cb7c423f61ac86f2842c2feb6e0a866f4
erhays <erhays@gmail.com>
2018-12-28 21:10:25 -0700

    i

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import UIkit from "uikit";
+// import UIkit from "uikit";
 
 Vue.config.productionTip = false
 
--- src/router.js
+++ src/router.js
@@ -3,12 +3,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Work from './views/Work.vue'
+import Blog from './views/Blog.vue'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
+  routes: [{
       path: '/',
       name: 'home',
       component: Home
@@ -19,7 +20,17 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: () => import( /* webpackChunkName: "about" */ './views/About.vue')
+    },
+    {
+      path: '/work',
+      name: 'work',
+      component: Work
+    },
+    {
+      path: '/blog',
+      name: 'blog',
+      component: Blog
     }
   ]
 })
